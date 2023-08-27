189a5acfe01d6aaa064fd07f6f4f7e6bb6ab38f0
noload <vaibhavkamble023@gmail.com>
2023-08-27 23:57:13 +0530

    Added DiagonalDifference program

---
+++ Problem Solving/DiagonalDifference.py
@@ -0,0 +1,92 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    count=0
+    leftToright=arr[0][0]+arr[1][1]+arr[2][2]
+    rightToleft=arr[0][2]+arr[1][1]+arr[2][0]
+    return abs(leftToright-rightToleft)
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+
+
+
+
+#methos 2
+
+
+    #!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    leftToright=0
+    rightToleft=0
+    for i in range(len(arr)):
+        leftToright+=arr[i][i]  
+    
+    for  j in range(len(arr)):
+        rightToleft+=arr[j][len(arr)-1-j]
+        
+    return abs(leftToright-rightToleft)
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
